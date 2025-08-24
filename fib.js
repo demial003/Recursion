85a0e0cb9099f206fb1ce9bcd227e3b08cf45493
demi <demial003@gmail.com>
2025-08-24 15:46:26 -0400

    Complete Fibonacci Implementation

---
+++ fib.js
@@ -0,0 +1,25 @@
+const fibs = (num) => {
+  let result = [0, 1];
+  for (let i = 2; i < num; ++i) {
+    let first = result[i - 2];
+    let second = result[i - 1];
+    result[i] = first + second;
+  }
+
+  return result;
+};
+
+console.log(fibs(4));
+
+const fibsRec = (num) => {
+  if (num < 1) return "invalid";
+  if (num === 1) return [0];
+  if (num === 2) return [0, 1];
+
+  return [
+    ...fibsRec(num - 1),
+    fibsRec(num - 1)[num - 2] + fibsRec(num - 2)[num - 3],
+  ];
+};
+
+console.log(fibsRec(0));
