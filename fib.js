02570c982e62fac36a588c9f76fc33498fb5fe5c
demi <demial003@gmail.com>
2025-08-25 13:03:05 -0400

    complete mergesort

--- fib.js
+++ fib.js
@@ -9,17 +9,17 @@ const fibs = (num) => {
   return result;
 };
 
-console.log(fibs(4));
+console.log(fibs(8));
 
 const fibsRec = (num) => {
-  if (num < 1) return "invalid";
+  if (num < 1) return [];
   if (num === 1) return [0];
   if (num === 2) return [0, 1];
 
   return [
     ...fibsRec(num - 1),
-    fibsRec(num - 1)[num - 2] + fibsRec(num - 2)[num - 3],
+    fibsRec(num - 1)[num - 2] + fibsRec(num - 1)[num - 3],
   ];
 };
 
-console.log(fibsRec(0));
+console.log(fibsRec(8));
---
+++ mergesort.js
@@ -0,0 +1,34 @@
+const merge = (left, right) => {
+  let i = 0;
+  let j = 0;
+  let res = [];
+
+  while (i < left.length1 || j < right.length) {
+    if (left[i] < right[j]) {
+      res.push(left[i]);
+      i++;
+    } else {
+      res.push(right[j]);
+      j++;
+    }
+  }
+
+  if (i < j) {
+    res = res.concat(left.slice(i, left.length));
+  } else {
+    res = res.concat(right.slice(j, right.length));
+  }
+  return res;
+};
+
+const mergeSort = (arr) => {
+  if (arr.length <= 1) return arr;
+  let mid = arr.length / 2;
+  let left = arr.slice(0, mid);
+  let right = arr.slice(mid, arr.length);
+
+  return merge(mergeSort(left), mergeSort(right));
+};
+
+console.log(mergeSort([3, 2, 1, 13, 8, 5, 0, 1]));
+console.log(mergeSort([105, 79, 100, 110]));
